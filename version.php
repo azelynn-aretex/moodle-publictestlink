0fd19355aa56be8fe5fb1694943d34381e0d09db
azelynn-aretex <azelynn-aretex@gmail.com>
2026-02-05 11:17:55 +1100

    Initialize

---
+++ version.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package public-test-link
+ * @author azi-team
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @var stdClass $plugin
+*/
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_message';
+$plugin->version = 202071900;
+$plugin->requires = 2016052300;
