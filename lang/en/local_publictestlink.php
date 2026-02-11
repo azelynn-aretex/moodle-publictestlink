aec1f82febfbc310a479e9236de8a2d25436fa20
azelynn-aretex <azelynn-aretex@gmail.com>
2026-02-12 08:53:33 +1100

    Add guard

--- lang/en/local_publictestlink.php
+++ lang/en/local_publictestlink.php
@@ -1,7 +1,11 @@
 <?php
+defined('MOODLE_INTERNAL') || die();
+
 $string['pluginname'] = 'Public Test Link';
+
 $string['makequizpublic'] = 'Make quiz public';
 $string['makequizpublic_desc'] = 'Allow anyone with the link to access this quiz without logging in';
+
 $string['publicquizsettings'] = 'Public Access Settings';
 $string['publicurl'] = 'Public Quiz URL';
 $string['publicurl_desc'] = 'Share this link to allow public access to the quiz';
