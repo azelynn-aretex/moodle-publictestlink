0855734d9a00951723169cadf2f8389ae7a4498a
azelynn-aretex <azelynn-aretex@gmail.com>
2026-02-12 10:22:35 +1100

    Implement quizcustom class

--- classes/quizcustom.php
+++ classes/quizcustom.php
@@ -5,51 +5,31 @@ use core\exception\moodle_exception;
 class publictestlink_quizcustom {
     public function __construct(
         protected int $id,
-        protected string $email,
-        protected string $firstname,
-        protected string $lastname
+        protected int $quizid,
+        protected bool $ispublic
     ) {}
 
-    public static function create(string $email, string $firstname, string $lastname): self {
+    public static function create(int $quizid, bool $ispublic): self {
         global $DB;
-    
-        $email = clean_email($email);
-
-        $firstname = clean_name($firstname);
-        $lastname = clean_name($lastname);
 
         $record = (object) [
-            'email' => $email,
-            'firstname' => $firstname,
-            'lastname' => $lastname,
-            'timecreated' => time()
+            'quizid' => $quizid,
+            'ispublic' => (int)$ispublic
         ];
-        $id = $DB->insert_record('local_publictestlink_shadowuser', $record);
+        $id = $DB->insert_record('local_publictestlink_quizcustom', $record);
 
         return new self(
-            $id, $record->email, $record->firstname, $record->lastname
+            $id, $record->quizid, $record->ispublic
         );
     }
 
     public static function from_id(int $id): ?self {
         global $DB;
-        $record = $DB->get_record('local_publictestlink_shadowuser', ['id' => $id], "*", IGNORE_MISSING);
+        $record = $DB->get_record('local_publictestlink_quizcustom', ['id' => $id], "*", IGNORE_MISSING);
         if (!$record) return null;
 
         return new self(
-            $record->id, $record->email, $record->firstname, $record->lastname
-        );
-    }
-
-    public static function from_email(string $email): ?self {
-        global $DB;
-        $email = clean_email($email);
-
-        $record = $DB->get_record('local_publictestlink_shadowuser', ['email' => $email], "*", IGNORE_MISSING);
-        if (!$record) return null;
-
-        return new self(
-            $record->id, $record->email, $record->firstname, $record->lastname
+            $record->id, $record->quizid, $record->ispublic === 1
         );
     }
 
@@ -57,32 +37,23 @@ class publictestlink_quizcustom {
         return $this->id;
     }
 
-    public function get_email(): string {
-        return $this->email;
+    public function get_quizid(): int {
+        return $this->quizid;
     }
 
-    public function get_firstname(): string {
-        return $this->firstname;
+    public function is_public(): string {
+        return $this->ispublic;
     }
 
-    public function get_lastname(): string {
-        return $this->lastname;
-    }
-
-    public function update_names(string $firstname, string $lastname) {
+    public function set_is_public(bool $ispublic) {
         global $DB;
         /** @var moodle_database $DB */
 
-        $firstname = clean_name($firstname);
-        $lastname = clean_name($lastname);
-
-        $DB->update_record('local_publictestlink_shadowuser', [
+        $DB->update_record('local_publictestlink_quizcustom', [
             'id' => $this->id,
-            'firstname' => $this->firstname,
-            'lastname' => $this->lastname,
+            'ispublic' => (int)$ispublic
         ]);
 
-        $this->firstname = $firstname;
-        $this->lastname = $lastname;
+        $this->ispublic = $ispublic;
     }
 }
