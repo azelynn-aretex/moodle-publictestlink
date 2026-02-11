898e7cf3a47805918eeac1c925c797714fd8ee83
azelynn-aretex <azelynn-aretex@gmail.com>
2026-02-11 17:20:54 +1100

    Add localized strings

--- lang/en/local_publictestlink.php
+++ lang/en/local_publictestlink.php
@@ -3,3 +3,12 @@ defined('MOODLE_INTERNAL') || die();
 
 $string['makequizpublic'] = 'Make quiz public';
 $string['makequizpublic_help'] = 'When enabled, this quiz can be accessed using a public link.';
+
+$string['form_email'] = 'Email Address';
+$string['form_firstname'] = 'First Name';
+$string['form_lastname'] = 'Last Name';
+
+$string['accesserror_quiznotopen'] = 'Quiz is not open for attempts yet';
+$string['accesserror_quizclosed'] = 'Quiz is closed';
+$string['accesserror_timelimitexpired'] = 'The time limit has been reached';
+$string['accesserror_maxattempts'] = 'You\'ve reached the maximum attempts for this quiz';
