c275c454ff422d19cc20a5a4fff7c91be4b6af5a
azelynn-aretex <azelynn-aretex@gmail.com>
2026-02-13 14:16:24 +1100

    Add bottom margin for user header

--- classes/user_header_writer.php
+++ classes/user_header_writer.php
@@ -9,7 +9,7 @@ class user_header_writer {
     public static function write(publictestlink_session $session) {
         $shadowuser = $session->get_user();
 
-        $html = html_writer::start_div('d-flex w-100 flex-row gap-4 justify-content-center align-items-center');
+        $html = html_writer::start_div('d-flex mb-4 w-100 flex-row gap-4 justify-content-center align-items-center');
             $html .= html_writer::tag('h6',
                 "Logged in as {$shadowuser->get_firstname()} {$shadowuser->get_lastname()} ({$shadowuser->get_email()})."
             );
